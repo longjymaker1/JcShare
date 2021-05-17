503a5a9bcfeac00d60a9972df0cc6e452c37525a
longjymaker1 <915268001@qq.com>
2021-05-17 17:45:45 +0800

    index

--- JcBBS/views.py
+++ JcBBS/views.py
@@ -9,6 +9,8 @@ import json
 def index(request):
     """主页"""
     if request.method == "GET":
-        return render(request, "index.html")
+        ats = dict(Articles_type.article_type)
+        print(ats)
+        return render(request, "index.html", {'types': ats})
 
 
