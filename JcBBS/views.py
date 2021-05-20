5ceafc2cfd48c3cf922ee6b481a7889401955474
longjymaker1 <915268001@qq.com>
2021-05-20 23:25:28 +0800

    index

--- JcBBS/views.py
+++ JcBBS/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Provinces, Cities, Block, Users,\
-    Articles_type, Articles,Article_msg
+    Articles_type, Articles, Article_msg
 from django.views.decorators.csrf import csrf_protect
 import json
 
@@ -10,7 +10,8 @@ def index(request):
     """主页"""
     if request.method == "GET":
         ats = dict(Articles_type.article_type)
-        print(ats)
-        return render(request, "index.html", {'types': ats})
+        pros = Provinces.objects.all()
+        print(pros)
+        return render(request, "index.html", {'types': ats, 'pros': pros})
 
 
