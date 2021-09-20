fbae69306232d215a55b0678d2175074bec516e6
longjymaker1 <915268001@qq.com>
2021-09-20 14:17:09 +0800

    主页

--- JcBBS/views.py
+++ JcBBS/views.py
@@ -7,34 +7,37 @@ from django.db import connection
 import json
 
 
-def index(request, pro_id=None):
+def index(request, pro_id=None, city_id=None, type_id=None):
     """主页"""
     if request.method == "GET":
-        ats = dict(Articles_type.article_type)
-        pros = Provinces.objects.all()
-        datas = {'types': ats, 'pros': {}}
-        for p in pros:
-            # print(p.id, p.name, p.is_ftc)
-            citis = Cities.objects.filter(province_id=p.id)
-            citis_list = []
-            if p.is_ftc == 0:
-                for c in citis:
-                    # print(c.name)
-                    citis_list.append((c.id, c.name))
-            else:
-                blocks = Block.objects.filter(city=citis[0].id)
-                for b in blocks:
-                    # print(b.name)
-                    citis_list.append((b.id, b.name))
-            datas['pros'][p.id] = [p.name, citis_list]
-        girls_data = get_index_data()
-        datas['girls'] = girls_data
+        datas = {
+            'types': dict(Articles_type.article_type),
+            'pros': get_address_data(),
+            'girls': get_index_data()
+        }
         return render(request, "index.html", datas)
     if request.method == "POST":
         if pro_id is None:
             print(pro_id)
         else:
             raise print("pro_id为空")
+
+
+def get_address_data():
+    pros = Provinces.objects.all()
+    tpros = {}
+    for p in pros:
+        citis = Cities.objects.filter(province_id=p.id)
+        citis_list = []
+        if p.is_ftc == 0:
+            for c in citis:
+                citis_list.append((c.id, c.name))
+        else:
+            blocks = Block.objects.filter(city=citis[0].id)
+            for b in blocks:
+                citis_list.append((b.id, b.name))
+        tpros[p.id] = [p.name, citis_list]
+    return tpros
 
 
 def get_index_data():
